3e988c3e228eea07fb0b6a25f0063b6940196931
DutchDS <dutchdatasmarts@gmail.com>
2019-10-23 06:07:21 -0500

    Preparing homework assignment

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,1 +1,35 @@
-'Newly created file by A. Rijpkema 11/12/19'
+# Define Variables
+
+period_count = 0 #Counter for Months
+total_profit = 0 #Sum of Value over all months
+change_profit = 0 #Sum of Vaue change for Month - Month-1
+avg_profit = 0 #Change / Months-1
+max_profit_change = 0 #Max of Change
+min_profit_change = 0 #Min of Change
+my_array = ["Month","Profit","Change"]
+
+#Read all records into an array
+
+
+#Loop through the array and update Variables
+#Months, Total change and Average Change
+#Append Average Change to new array
+
+#while i <= period_count:
+
+
+
+#Loop through my array and find greatest increase and decrease
+
+
+
+#Print outcome to screen
+print("Financial Analysis")
+print("-------------------------------------------------")
+print(f"Total Months: {period_count}")
+print(f"Total: {total_profit}")
+print(f"Average Change: {avg_profit}")
+print(f"Greatest Increase in Profits: {max_profit_change}")
+print(f"Greatest Decrease in Profits: {min_profit_change}")
+
+#Write results into file
