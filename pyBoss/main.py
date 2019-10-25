224a708e0c80272b6b83402f432ea07b7ba06188
DutchDS <dutchdatasmarts@gmail.com>
2019-10-25 13:10:11 -0500

    Almost there :-)

--- pyBoss/main.py
+++ pyBoss/main.py
@@ -2,6 +2,8 @@
 import os
 import csv
 
+#import pandas as pd
+
 employee_data_path = os.path.join('..','Resources', 'employee_data.csv')
 output_data_file = os.path.join('..','Output',"new_employee_data.csv")
 
@@ -11,8 +13,13 @@ myFirst = "TBD"
 myLastName = []
 myLast = "TBD"
 myDOB = []
+#myDOBnew = "TBD"
 mySSN = []
 myState = []
+
+# def type_convert(var):
+#     if len(var) == 10:                 
+#         return pd.to_datetime(var, format='%m/%d/%Y').date()
 
 with open(employee_data_path, newline="") as csvfile:
     csvreader = csv.reader(csvfile , delimiter=",")
@@ -23,8 +30,15 @@ with open(employee_data_path, newline="") as csvfile:
         myFirstName.append(myFirst)
         myLast = row[1].split()[1]
         myLastName.append(myLast)
-        myDOB.append(row[2])
-        mySSN.append(row[3])
+        myDOBcheck = row[2]
+        myDOByear = row[2][0:4]
+        myDOBmonth = row[2][5:7]
+        myDOBday = row[2][8:10]
+        myDOBnew = (myDOBmonth + "/" + myDOBday + "/" + myDOByear)
+        myDOB.append(myDOBnew)
+        mySSNnew = row[3][7:12]
+        mySSNreplace = ('***-**-' + mySSNnew)
+        mySSN.append(mySSNreplace)
         myState.append(row[4])
 
 cleaned_employee_data = zip(myEmp_ID,myFirstName,myLastName,myDOB,mySSN,myState)
@@ -33,7 +47,7 @@ with open(output_data_file, "w", newline="") as datafile:
     writer = csv.writer(datafile)
 
     # Write the header row
-    #writer.writerow(["Emp ID,First Name,Last Name,DOB,SSN,State"])
+    writer.writerow(['Emp ID','First Name','Last Name','DOB','SSN','State'])
 
     # Write in zipped rows
     writer.writerows(cleaned_employee_data)
